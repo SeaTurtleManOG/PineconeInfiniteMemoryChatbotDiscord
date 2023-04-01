612eab35b95e5acca4f7f0f31b12a25805004650
SeaTurtleManOG <70815193+SeaTurtleManOG@users.noreply.github.com>
2023-04-02 03:46:03 +0800

    Update gpt3_functions.py

--- gpt3_functions.py
+++ gpt3_functions.py
@@ -2,10 +2,11 @@ import openai
 import os
 import json
 import re
+from datetime import datetime
 from time import time
 import asyncio
-from config import config
 import pinecone
+from config import config
 import uuid
 from uuid import uuid4
 import requests
@@ -14,7 +15,21 @@ from nltk.tokenize import word_tokenize
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import nltk
+import faiss
+from pinecone_utils import load_vectors_from_pinecone_to_faiss, load_conversation
 
+# Faiss index initialization
+dimension = 1536  # GPT-3 embedding dimension for Faiss
+faiss_index = faiss.IndexFlatL2(dimension)
+
+def save_faiss_index(filename):
+    faiss.write_index(faiss_index, filename)
+
+def load_faiss_index(filename):
+    global faiss_index
+    faiss_index = faiss.read_index(filename)
+
+index_to_filename_mapping = {}
 
 from utility_functions import (
     open_file,
@@ -27,39 +42,43 @@ from utility_functions import (
 )
 nltk.download('punkt')
 
-
-
-
 def read_api_key(filename):
     with open(filename, 'r') as file:
         return file.read().strip()
+
+default_vector = [0] * 1536
+faiss_results = []
 
 # Initialize openai
 openai.api_key_path = "key_openai.txt"
 pinecone_api_key = os.getenv("PINECONE_API_KEY")
 pinecone.init(api_key=pinecone_api_key, environment="us-west4-gcp")
-vdb = pinecone.Index(index_name="yourindexname")
-
+vdb = pinecone.Index(index_name="turtle-history")
 
 # Function to load conversation history based on user_id and other parameters
-def load_conversation(results, user_id, max_tokens=2000, max_messages=None):
+def load_conversation(faiss_results, pinecone_results, user_id, max_tokens=2000, max_messages=None):
     result = list()
-    for m in results['matches']:
-        info = load_json('nexus/%s.json' % m['id'])
-        if info.get('user_id') == user_id:  # Filter messages by user ID using the get() method
-            result.append(info)
+    fetched_vectors = vdb.fetch(ids=[m["id"] for m in pinecone_results['matches']])
+    for m in pinecone_results['matches']:
+        file_path = f'nexus/{m["id"]}.json'
+        if os.path.exists(file_path):  # Add this check to see if the file exists
+            info = load_json(file_path)
+            info["vector"] = fetched_vectors[m["id"]]  # Replace the loaded vector with the fetched vector
+            if info.get('user_id') == user_id or info.get('speaker') == 'RAVEN':
+                result.append(info)
     ordered = sorted(result, key=lambda d: d['time'], reverse=False)
 
     if max_messages:
         ordered = ordered[-max_messages:]
 
-    messages = [i['message'] for i in ordered]
+    messages = [i['message'] for i in ordered if i['message'] is not None]  # Filter out None values
     message_block = '\n'.join(messages).strip()
 
     if len(message_block.split()) > max_tokens:
         message_block = summarize_conversation(message_block, max_tokens)
 
     return message_block
+
 # SUmmarize function
 async def summarize_website(url):
     if "twitter.com" in url:
@@ -93,9 +112,26 @@ async def summarize_website(url):
 
     return summary
 
+# Function to generate an embedding vector for a given text using GPT-3
+def gpt3_embedding(content, engine='text-embedding-ada-002'):
+    if not content:
+        # Handle the empty string case here, for example, return a default vector
+        return default_vector
+    content = content.encode(encoding='ASCII', errors='ignore').decode()
+    response = openai.Embedding.create(input=content, engine=engine)
+    vector = response['data'][0]['embedding']
+
+    # Ensure the vector has the correct length (1536)
+    if len(vector) != 1536:
+        print(f"Warning: The vector length is {len(vector)}, expected 1536.")
+        vector = vector[:1536] + [0] * (1536 - len(vector))
+
+    return vector
+
 
 # Function to process user input and generate a response
 async def process_user_input(user_input, user_id, convo_length=10):
+    response = None
     max_tokens = 4096  # Define the maximum tokens allowed for GPT-3
 
     # Truncate user_input if it's too long
@@ -124,12 +160,39 @@ async def process_user_input(user_input, user_id, convo_length=10):
     vector = gpt3_embedding(message)
     unique_id = str(uuid4())
     metadata = {'speaker': 'USER', 'time': timestamp, 'message': message, 'uuid': unique_id}
-    save_json('nexus/%s.json' % unique_id, metadata)
+    save_json(f'nexus/{unique_id}.json', metadata)
     payload.append((unique_id, vector))
 
-    # Search for relevant messages and generate a response
-    results = vdb.query(vector=vector, top_k=convo_length)
-    conversation = load_conversation(results, user_id)  # Add user_id parameter
+    # Search for relevant messages in Pinecone and Faiss
+    pinecone_results = vdb.query(vector=vector, top_k=convo_length)
+    
+    # Store the bot's response and user messages in Pinecone
+    vector_response = gpt3_embedding(response) if response is not None else vector
+    unique_id_response = str(uuid.uuid4())
+    metadata_response = {'speaker': 'RAVEN', 'time': time(), 'message': response, 'uuid': unique_id_response}
+
+    # Save the metadata for the response
+    save_json(f'nexus/{unique_id_response}.json', metadata_response)
+
+    # Upsert the bot's response and user messages into Pinecone
+    vdb.upsert([(unique_id_response, vector_response), (unique_id, vector)])
+
+    # Update Faiss index
+    update_faiss_index(unique_id, vector)
+    
+    # Generate a response and time delay using GPT-3
+    response = (await gpt3_completion(prompt, user_id, config.get("CONVO_LENGTH", 10))).strip()
+    
+    # Check if the conversation is in the cache
+    if user_id in cache:
+        conversation = cache[user_id]
+    else:
+        conversation = load_conversation(faiss_results, pinecone_results, user_id)
+
+    # Truncate conversation if it's too long
+    tokens = word_tokenize(conversation)
+    if len(tokens) > max_tokens:
+        conversation = ' '.join(tokens[:max_tokens])
 
     system_message = config["CUSTOM_SYSTEM_MESSAGES"].get(str(user_id), config.get("SYSTEM_MESSAGE"))
     prompt = f"System message: {system_message}\n\nPREVIOUS CONVERSATION:\n\n{conversation}\n\nUSER: {user_input}\n"
@@ -148,16 +211,20 @@ async def process_user_input(user_input, user_id, convo_length=10):
     info = {'speaker': 'RAVEN', 'time': timestamp, 'vector': vector, 'message': message, 'uuid': str(uuid4())}
     payload.append((unique_id, vector))
     vdb.upsert(payload)
-
+    
+    # Save the vector to both Pinecone (1536-dimensional) and Faiss (768-dimensional)
+    vector_768 = gpt3_embedding_768(message)
+    vector_768_np = np.array(vector_768).reshape(1, -1)  # Convert the list to a NumPy array and reshape
+    faiss_index.add(vector_768_np)
+    vdb.upsert([(unique_id, vector)])
+    
+    
     # Return the output
     return output
     
-# Function to generate an embedding vector for a given text using GPT-3
-def gpt3_embedding(content, engine='text-embedding-ada-002'):
-    content = content.encode(encoding='ASCII', errors='ignore').decode()
-    response = openai.Embedding.create(input=content, engine=engine)
-    vector = response['data'][0]['embedding']
-    return vector
+    # Clear the cache for the current user
+    if user_id in cache:
+        del cache[user_id]
 
 # Function to generate a completion using GPT-3 with the given prompt and other parameters
 async def gpt3_completion(prompt, user_id=None, model='gpt-3.5-turbo', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['USER:', 'RAVEN:']):
@@ -166,7 +233,7 @@ async def gpt3_completion(prompt, user_id=None, model='gpt-3.5-turbo', temp=0.7,
     prompt = prompt.encode(encoding='ASCII', errors='ignore').decode()
     while True:
         try:
-            system_message = config["CUSTOM_SYSTEM_MESSAGES"].get(str(user_id), config.get("SYSTEM_MESSAGE", "You are a helpful assistant."))
+            system_message = config["CUSTOM_SYSTEM_MESSAGES"].get(str(user_id), config.get("SYSTEM_MESSAGE"))
             messages = [{"role": "system", "content": system_message}]
             if prompt.startswith("USER:"):
                 messages.append({"role": "user", "content": prompt[5:]})
